5f34cd136b81fc7f3c4c3c9616dc8625010e03df
crisjc6 <cjumbocu@everis.com>
2022-07-12 08:43:22 -0500

    update mock service

--- src/data/repository.mock.ts
+++ src/data/repository.mock.ts
@@ -2,15 +2,15 @@ export const REPOSITORIES =
 {
     "repositories": [
         {
-            "id": 1,
+            "id": "778314964425605121",
             "state": 604
         },
         {
-            "id": 2,
+            "id": "778318239851773954",
             "state": 605
         },
         {
-            "id": 3,
+            "id": "3",
             "state": 606
         }
     ]
